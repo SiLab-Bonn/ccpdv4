20655ce6fdf6d4a3621e089e836467361eb1f560
Jens Janssen <janssen@physik.uni-bonn.de>
2015-03-05 15:36:49 +0100

    ENH: adding all VSRC

--- ccpdv4/ccpdv4_run_base.py
+++ ccpdv4/ccpdv4_run_base.py
@@ -15,7 +15,14 @@ class Ccpdv4RunBase(Fei4RunBase):
         self.ccpd_register.write_pixel()
 
     def power_on(self):
-        self.dut['V_in'].set_current_limit(1000, unit='mA')  # one for all
+        # PWR
+        self.dut['V_in'].set_current_limit(1000, unit='mA')  # one for all, max. 1A
+        # V_in
+#         self.dut['V_in'].set_voltage(2.1, unit='V')
+#         self.dut['V_in'].set_enable(True)
+#         if self.dut["V_in"].get_over_current():
+#             self.power_off()
+#             raise Exception('V_in overcurrent detected')
         # Vdd, also enabling LVDS transceivers
         self.dut['CCPD_Vdd'].set_voltage(1.8, unit='V')
         self.dut['CCPD_Vdd'].set_enable(True)
@@ -34,25 +41,31 @@ class Ccpdv4RunBase(Fei4RunBase):
         if self.dut["CCPD_VGate"].get_over_current():
             self.power_off()
             raise Exception('VGate overcurrent detected')
-        # Vcasc
-        self.dut['CCPD_Vcasc'].set_voltage(1.1, unit='V')
-        # enabling V_in
-        self.dut['V_in'].set_voltage(2.1, unit='V')
-        self.dut['V_in'].set_enable(True)
-        if self.dut["V_in"].get_over_current():
-            self.power_off()
-            raise Exception('V_in overcurrent detected')
+        # VSRC
+        self.dut["CCPD_Vcasc"].set_voltage(0.0, unit="V")  # TODO: fix value
+        self.dut["CCPD_HVPlus"].set_voltage(0.0, unit="V")  # TODO: fix value
+        self.dut["CCPD_BL"].set_voltage(0.0, unit="V")  # TODO: fix value
+        self.dut["CCPD_Th"].set_voltage(0.0, unit="V")  # TODO: fix value
+        # ISRC
+        self.dut["CPPD_VN"].set_current(0.0, unit="uA")  # TODO: fix value
 
     def power_off(self):
-        self.dut['CCPD_VGate'].set_enable(False)
-        self.dut['CCPD_Vssa'].set_enable(False)
-        self.dut['CCPD_Vdd'].set_enable(False)
-        self.dut['V_in'].set_enable(False)
+        # VSRC
         self.dut["CCPD_Vcasc"].set_voltage(0.0, unit="V")
+        self.dut["CCPD_HVPlus"].set_voltage(0.0, unit="V")
+        self.dut["CCPD_BL"].set_voltage(0.0, unit="V")
+        self.dut["CCPD_Th"].set_voltage(0.0, unit="V")
+        # ISRC
+        self.dut["CPPD_VN"].set_current(0.0, unit="uA")
+        # PWR
         self.dut["CCPD_VGate"].set_voltage(0.0, unit="V")
         self.dut["CCPD_Vssa"].set_voltage(0.0, unit="V")
         self.dut["CCPD_Vdd"].set_voltage(0.0, unit="V")
         self.dut["V_in"].set_voltage(0.0, unit="V")
+        self.dut['CCPD_VGate'].set_enable(False)
+        self.dut['CCPD_Vssa'].set_enable(False)
+        self.dut['CCPD_Vdd'].set_enable(False)
+        self.dut['V_in'].set_enable(False)
 
     def init_dut(self):
         self.power_on()
