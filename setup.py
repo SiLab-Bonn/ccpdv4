5593462facb5fc57466307bf51c5d3faff0da9cd
Jens Janssen <janssen@physik.uni-bonn.de>
2016-02-18 20:47:42 +0100

    MAINT: fix all dependencies

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     maintainer=author,
     author_email=author_email,
     maintainer_email=author_email,
-    install_requires=['pyBAR>=2.1.0', 'basil==2.4.2'],
+    install_requires=['pyBAR>=2.1.0dev', 'basil_daq==2.4.2'],
     packages=find_packages(),  # exclude=['*.tests', '*.test']),
     include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
     package_data={'': ['README.*', 'VERSION'], 'docs': ['*'], 'examples': ['*'], 'ccpdv4': ['*.yaml', '*.bit']},
